16facf1fbb9aad23b47486ae1c9de2f7ec3a2407
wangbo <99548050@qq.com>
2026-04-23 14:51:57 +0800

    feat: ✨ 完善token

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { setStorage } from '@/utils/storage'
+
+  function handleLogin() {
+    setStorage('TOKEN', '123456')
+    uni.navigateTo({
+      url: '/pages/dashboard/index'
+    })
+  }
+</script>
 
 <template>
   <view px-10 py-20 text-center>
-    <view class="text-2xl font-bold">登录</view>
+    <view class="text-2xl font-bold" @click="handleLogin">登录</view>
+    <tButton @click="handleLogin">登录</tButton>
   </view>
 </template>
